58efc4b23b21809bba3f31d9a6268d115ea7f192
Iliana Gonzalez <ilianapgh@icloud.com>
2022-08-29 17:56:23 -0500

    mi primera version

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+const container = document.querySelector('.navbar__container')
+const menu = document.getElementById('menu')
+console.log(container)
+
+  container.addEventListener('click', function (e) {
+    
+    if(e.target.matches('.menu--open')) {
+        menu.classList.add('show--menu')
+    }
+
+    if(e.target.matches('.menu--close')) {
+        menu.classList.remove('show--menu')
+    }
+
+    if (e.target.matches('menu__link')) {
+      menu.classList.remove('show--menu')
+    }
+  })
+
